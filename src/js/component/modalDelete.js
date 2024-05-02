252c5ef6929a23d0ea41d4c65911435c4b8d8f6a
Marta Haro <143503919+Martharoca@users.noreply.github.com>
2024-05-02 10:03:42 +0000

    working in modalDelete

---
+++ src/js/component/modalDelete.js
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext.js";
+import "../../styles/index.css";
+
+
+export const ModalDelete = ({ setModal, stateModal, contact }) => {
+
+    const { store, actions } = useContext(Context)
+
+     function handleDelete() { 
+         actions.deleteContact(store.contactToDelete.id)
+         setModal({ show: "none" })
+     }
+
+
+    return (
+        <div>
+            <div className="modal" tabIndex="-1" role="dialog" style={{ display: stateModal.show }}>
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Are you sure?</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close"
+                                onClick={() => { setModal({ show: "none" }) }}
+                            >
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <p>If you delete this thing the entire universe will go down!</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-primary" onClick={() => { setModal({ show: "none" }) }}>Oh no!</button>
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal"
+                                onClick={handleDelete}>Yes baby!</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,6 +1,6 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
-		store: {
+		store: { // ESTADOS globales
 			demo: [
 				{
 					title: "FIRST",
@@ -12,32 +12,68 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			contacts: [],
+			contact: {},
+			contactToDelete: {}
 		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+		actions: { // FUNCIONES globales
+			createContact: function (contact) {
+				fetch("https://playground.4geeks.com/contact/agendas/marta992/contacts", {
+					method: 'POST',
+					headers: {
+						"content-type": "application/json"
+					},
+					body: JSON.stringify({
+						"name": contact.name,
+						"phone": contact.phone,
+						"email": contact.email,
+						"address": contact.address
+					})
+				})
+					.then((response) => response.json())
+					.then((data) => {
+						if (data.ok) {
+							getActions().getAllContacts()
+						}
+					})
+					.catch((error) => console.log(error))
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+
+			getAllContacts: function () {
+				fetch('https://playground.4geeks.com/contact/agendas/marta992/contacts')
+					.then((response) => {
+						console.log(response);
+						if (response.status === 404) {
+							getActions().createUser()
+						}
+						return response.json()
+					})
+					.then((data) => setStore({ contacts: data.contacts }))
+					.catch((error) => console.log(error))
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			deleteContact: function(contact) {
+				fetch(`https://playground.4geeks.com/contact/agendas/marta992/contacts/${id}`, {
+					method: 'DELETE',
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					 .then((response) => { if (response.ok) getActions().getAllContacts() })
+					 .catch((error) => console.log(error))
+			},
+			createUser: function () {
+				fetch('https://playground.4geeks.com/contact/agendas/marta992', {
+					method: 'POST',
+					body: JSON.stringify(""),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then((response) => response.json())
+					.then((data) => console.log(data))
+					.catch((error) => console.log(error))
+			},
 		}
 	};
 };
